e2820d860ad84e1b8ecaaba61d4ed8140e777f6e
harisnasir1 <haris.nasir0101@gmail.com>
2025-06-27 11:24:29 +0100

    fix coast per item in order review

--- Backend/Routes/OrderReviewRoute.js
+++ Backend/Routes/OrderReviewRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {updateorders}=require("../Controllers/OrderReviewController")
+const {updateorders,getshopifyorders}=require("../Controllers/OrderReviewController")
 
 router.post("/UpdateReview",updateorders);
-
+router.post("/getshopifyorders", getshopifyorders);
 module.exports = router;
