f23175126fcfabbc4158b92c1ae128d213090803
harisnasir1 <haris.nasir0101@gmail.com>
2025-07-10 18:09:06 +0100

    fix console messages

--- Backend/Routes/S3routes.js
+++ Backend/Routes/S3routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {presignedurl}=require("../Controllers/S3Controller")
+const {presignedurl, backupDatabase}=require("../Controllers/S3Controller")
 
 router.post("/presignedurl",presignedurl);
+router.get("/backup", backupDatabase);
 
 module.exports= router;
